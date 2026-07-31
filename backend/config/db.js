c1e41eb565626d453bb202ecb300fdb2904916cc
anjali752 <anjalideoria13@gmail.com>
2026-07-31 17:31:58 +0530

    Updated for Vercel

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnect = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("Database connected successfully...");
+        console.log("Database connected successfully.");
     } catch (error) {
         console.log(error);
     }
